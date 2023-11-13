841eda20681e6a29b01f35c2b01218e53d395ae8
RubenIrimiaPerez <rubenbaloncesto10@gmail.com>
2023-11-13 13:20:36 +0100

    AlertDialog creado

--- app/src/main/java/com/example/eventup/Login.java
+++ app/src/main/java/com/example/eventup/Login.java
@@ -1,10 +1,14 @@
 package com.example.eventup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
 
@@ -13,5 +17,23 @@ public class Login extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+    }
+    public void AlertDialog(View view){
+        androidx.appcompat.app.AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        EditText nuevaContraeña = new EditText(this);
+        nuevaContraeña.setInputType(InputType.TYPE_CLASS_TEXT);
+        nuevaContraeña.setHint("nuevamail@example.com");
+
+        builder.setView(nuevaContraeña);
+        builder.setTitle("Ingrese el e-mail para poder reestablecer contraseña");
+
+        builder.setPositiveButton("Confirmar", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(Login.this, "Enviado", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
     }
 }
--- app/src/main/java/com/example/eventup/MainActivity.java
+++ app/src/main/java/com/example/eventup/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.eventup;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +18,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
 }
