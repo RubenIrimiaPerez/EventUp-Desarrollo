04cae85d39d97418524c1b63a13cc26da461dd92
RubenIrimiaPerez <rubenbaloncesto10@gmail.com>
2023-11-13 12:46:35 +0100

    Login

---
+++ app/src/main/java/com/example/eventup/FirstDesignApplication.java
@@ -0,0 +1,13 @@
+package com.example.eventup;
+
+import android.app.Application;
+
+import com.google.android.material.color.DynamicColors;
+
+public class FirstDesignApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        DynamicColors.applyToActivitiesIfAvailable(this);
+    }
+}
---
+++ app/src/main/java/com/example/eventup/SplashScreen.java
@@ -0,0 +1,55 @@
+package com.example.eventup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splah_screen);
+        openLogin();
+
+        ImageView logoSplash = findViewById(R.id.logoSplash);
+        TextView appNameSplash = findViewById(R.id.appNameSplash);
+
+        Animation fadeAnimation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        appNameSplash.startAnimation(fadeAnimation);
+
+        Animation shakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
+        logoSplash.startAnimation(shakeAnimation);
+
+        ImageView background = findViewById(R.id.backgroundSplash);
+        Glide.with(this)
+                .load("https://images.unsplash.com/photo-1565035010268-a3816f98589a?q=80&w=1976&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D")
+                .transition(DrawableTransitionOptions.withCrossFade(2000))
+                .centerCrop()
+                .into(background);
+    }
+
+    public void openLogin(){
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+            }
+        }, 3000);
+    }
+    }
+
+
